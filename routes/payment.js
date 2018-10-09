61f299502dc4856a3f345351a0df626afed2bf07
netsplashchat <monu_kanyal@esferasoft.com>
2018-10-09 11:51:16 +0530

    updated1

--- routes/payment.js
+++ routes/payment.js
@@ -69,7 +69,7 @@ router.post('/c2b_pay', function (req, res) {
                                         //Fill in the request parameters with valid values
                                         "ShortCode": "602980",
                                         "CommandID": "CustomerBuyGoodsOnline",
-                                        "Amount": req.body.price,
+                                        "Amount": ParseInt(req.body.price),
                                         "Msisdn": req.body.mobilenum,
                                         "BillRefNumber": nanoid()
                                     }
